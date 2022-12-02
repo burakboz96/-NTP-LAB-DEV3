0fc9ce78fbaf2fa918fedf4ef7116d605102d45d
burak bozoğlu <burakbozoglu96@gmail.com>
2022-12-02 18:00:38 +0300

    Add files via upload

---
+++ ödev3/ödev3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev3
+{
+    internal class Program
+    {
+
+        /// <summary>     örnek lab uygulama ödevi öğrenci bilgilerinin bellekte kaydedildiği ekren çıktısı
+        /// ////////////////////////////////////////////////////////////////
+        /// </summary>
+          
+
+        // static void Main(string[] args)
+        // {
+
+        //  Console.WriteLine("EN FAZLA 5 ÖĞRENCİ GİRİNİZ...");
+        //  Console.WriteLine();
+        //Console.WriteLine("İşlemler");
+        //Console.WriteLine("********");
+        //     Console.WriteLine(" ");
+        //     string[] ogrenci = new string [6];
+
+
+        //     double ortalama;
+
+
+        //Console.WriteLine("1- öğrenci ekle   2- Öğrenci sayısını göster   3- Ortalamayı göster");
+        //     Console.WriteLine(" ");
+        //     Console.WriteLine("SEÇİMİNİZ: ");
+
+        //     int secim = int.Parse(Console.ReadLine());      
+
+
+
+
+        //     if (secim == 1) { 
+
+        //     Console.WriteLine("ÖĞRENCİLERİN İSMİNİ GİRİN");
+        //     for(int sayı = 1; sayı < ogrenci.Length; sayı++) {
+        //             Console.WriteLine("");
+        //         Console.WriteLine(sayı+". İSMİ YAZ ");
+
+        //         string isim=Console.ReadLine();
+
+        //     }
+        //   }
+
+        //     if(secim == 2) Console.WriteLine("girilen öğrenci sayısı= " + ogrenci.Length);{
+        //         Console.WriteLine();
+        //      }
+
+
+        //     if (secim == 3)
+        //     {
+        //         int toplam = 0;
+        //         for (int i = 1; i < ogrenci.Length; i++)
+        //         {
+        //             Console.WriteLine("");
+        //             Console.WriteLine( "ÖĞRENCİLERİN NOTLARINI YAZINIZ... ");
+
+        //            int notgir=Convert.ToInt32(Console.ReadLine());
+        //              toplam += notgir;
+
+
+        //         }
+        //         Console.WriteLine("Ortalama= "+toplam/5) ;
+        //         Console.ReadKey(); 
+
+        //     }
+
+        //      if(3<secim || 0>secim)
+        //     {
+        //         Console.WriteLine("LÜTFEN BELİRTİLEN OPSİYONLARI SEÇİNİZ");
+        //     }
+
+
+
+
+
+        ////////////////////////////////////////////////////////////////
+
+
+
+
+
+        // } lab ugulama ödevi 2
+
+
+        // 1 ‘den 100 e kadar sayıların toplamı
+
+        //class Topla
+        //{
+        //    public int toplam;
+        //    public Topla(int toplam)
+
+        //    { 
+        //        toplam = toplam; }
+        //    public int Toplama
+        //    {
+        //        get { return toplam; } 
+        //        set
+        //        {
+        //            toplam = value + toplam;
+        //        } 
+        //    }
+
+        //    static void Main(string[] args)
+        //    {
+        //        Topla ekle = new Topla(0);
+        //        Console.WriteLine("Başlangıç değeri->" + ekle.Toplama);
+        //        for (int i = 1; i < 100; i++) ekle.Toplama = i;
+        //        Console.WriteLine("Sayıların toplamı" + ekle.Toplama);
+
+
+
+        //    }
+       // }
+
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////
+        
+
+
+    }
+}
---
+++ ödev3/ödev3/sinif.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ödev3
+{
+    internal class sinif
+    {
+        public  sinif()
+        {
+     
+
+
+
+        }
+    }
+}
